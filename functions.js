a1e4825152b3fcaf85b6d427d42b271f88e21130
Leo Chung <leo.chung97@gmail.com>
2023-03-07 13:50:09 -0500

    Add JavaScript files from old code and npm install puppeteer

---
+++ functions.js
@@ -0,0 +1,36 @@
+export function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
+export function title_prep(string) {
+  return toTitleCase(string).replace(/-/g, " ");
+}
+
+export function url_prep(string) {
+  return string.toLowerCase().replace(/ /g, "-");
+}
+
+export function relevantTimes(time) {
+  const timeObj = new Date(`1970-01-01 ${time}`);
+  const roundedTime = new Date(
+    Math.floor(timeObj / (15 * 60 * 1000)) * (15 * 60 * 1000)
+  );
+
+  const relevantTimes = [];
+  for (let i = -2; i <= 2; i++) {
+    if (i == 0) {
+      continue;
+    }
+    const relevantTime = new Date(roundedTime.getTime() + i * 15 * 60 * 1000);
+    const formattedTime = relevantTime.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+    relevantTimes.push(formattedTime);
+  }
+
+  return relevantTimes;
+}
